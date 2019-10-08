0098c94cef718739d2519d50d23135ccb25514a8
rosita bannert <rosita.bannert@concentra.co.uk>
2019-10-08 15:58:49 +0100

    initial commit

---
+++ src/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="container">
+      <div className="rectangle-side">rectangle-side</div>
+      <div className="rectangle-top">top</div>
+      <div className="diagonal">diagonal</div>
+      <div className="square-small">square</div>
+      <div className="polygon">polygon</div>
+      <div className="circle">circle</div>
+    </div>
+  );
+};
+
+export default Homepage;
