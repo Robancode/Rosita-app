2880af53ff88da7efbc4f9b56c2123449178d9c9
rosita bannert <rosita.bannert@concentra.co.uk>
2019-10-08 18:11:29 +0100

    hooks

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Homepage = () => {
+  const [position, setPosition] = useState(9);
+  // need to keep position between 1 and 9
   return (
     <div className="container">
       <div className="rectangle-side"></div>
       <div className="rectangle-top"></div>
       <div className="diagonal"></div>
-      <div className="square-small"></div>
+      <div className="square-small">
+        <button onClick={() => setPosition(position => position + 1)}>
+          <IoIosArrowDown />
+        </button>
+        <button onClick={() => setPosition(position => position - 1)}>
+          <IoIosArrowUp />
+        </button>
+      </div>
       <div className="polygon"></div>
-      <div className="circle"></div>
+      <div className="circle" style={{ gridRowStart: position }}></div>
       <div className="footer"></div>
     </div>
   );
