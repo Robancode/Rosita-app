0b90d49b2e058f3adbd618549ed95c7e8b25a4ab
rosita bannert <rosita.bannert@concentra.co.uk>
2019-10-08 16:24:43 +0100

    css updates

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Homepage = () => {
   return (
     <div className="container">
-      <div className="rectangle-side">rectangle-side</div>
-      <div className="rectangle-top">top</div>
-      <div className="diagonal">diagonal</div>
-      <div className="square-small">square</div>
-      <div className="polygon">polygon</div>
-      <div className="circle">circle</div>
+      <div className="rectangle-side"></div>
+      <div className="rectangle-top"></div>
+      <div className="diagonal"></div>
+      <div className="square-small"></div>
+      <div className="polygon"></div>
+      <div className="circle"></div>
     </div>
   );
 };
