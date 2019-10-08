33c7fa95db2a80419f02552ba2a02e932ebb3f26
rosita bannert <rosita.bannert@concentra.co.uk>
2019-10-08 20:06:19 +0100

    Co-authored-by: Sean Rivard - Morton <Sean.rivard.morton@gmail.com> passing state

--- src/PositionCounter.js
+++ src/PositionCounter.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 
-const PositionCounter = ({ min = 1, max = 9 }) => {
-  const [position, setPosition] = useState(9);
+const PositionCounter = ({ min = 0, max = 9, useHomePage }) => {
+  // This isn't a hook, it's just array destructuring from the parent
+  const [position, setPosition] = useHomePage;
   const increment = () => {
-    if (position + 1 <= max) {
-      setPosition(position + 1);
-    }
+    if (position + 1 > max) return;
+    setPosition(position + 1);
   };
   const decrement = () => {
-    if (position - 1 <= min) {
-      setPosition(position - 1);
-    }
+    if (position - 1 < min) return;
+    setPosition(position - 1);
   };
   return (
     <div>
-      <button onClick={decrement}>
+      <button onClick={increment}>
         <IoIosArrowDown />
       </button>
-      <button onClick={increment}>
+      <button onClick={decrement}>
         <IoIosArrowUp />
       </button>
     </div>
